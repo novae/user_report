97f6bfad1ab6338bdc8acdb209ef6194e09d4cff
novaegr <novaegr@gmail.com>
2012-12-10 19:17:18 -0600

    rails g

---
+++ user_report/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UserReport::Application.config.secret_token = 'f8fa5cc7d1e80f0e4ca3040f60533c99f1e1530039a595d7926aced04635b9757e240268e19bf3965155b4bb394ea1a45b2985768e6cfff5d356cd51be206b5b'
